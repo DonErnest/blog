2412e350f2c7fcc72e4adca8a1b4af0a5d8d9d3c
DonErnest <valeetmihifaveas@gmail.com>
2019-10-01 15:21:22 +0600

    создание CRUD комментария с помощью generic классов джанго. Добавление ссылки на добавление комментария и на список комментариев на главную страницу, шаблон списка комментариев, частичный шаблон списка, шаблоны CRUD

--- to_do_list_src/to_do_list/urls.py
+++ to_do_list_src/to_do_list/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.urls import path
 
 from webapp.views import IndexView, ArticleView, ArticleCreateView, \
-    IndexRedirectView, ArtUpdateView, ArtDeleteView
+    IndexRedirectView, ArtUpdateView, ArtDeleteView, CommentAddView, CommentListView, CommentEditView, CommentDeleteView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -13,4 +13,8 @@ urlpatterns = [
     path('articles/add', ArticleCreateView.as_view(), name = 'article_add'),
     path('article/<int:pk>/update', ArtUpdateView.as_view(), name='article_update'),
     path('article/<int:pk>/delete', ArtDeleteView.as_view(), name="article_delete"),
+    path('comments/add/', CommentAddView.as_view(), name='add comment'),
+    path('comments/', CommentListView.as_view(), name='comments list'),
+    path('comment/<int:pk>/edit/', CommentEditView.as_view(), name='edit comment'),
+    path('comment/<int:pk>/', CommentDeleteView.as_view(), name='delete comment'),
 ]
--- to_do_list_src/webapp/forms.py
+++ to_do_list_src/webapp/forms.py
@@ -1,9 +1,18 @@
 from django import forms
 from django.forms import widgets
 
+from webapp.models import Comment
+
 
 class ArticleForm(forms.Form):
     title = forms.CharField(max_length=200, label = 'Title', required= True)
     author = forms.CharField(max_length=40, label='Author', required=True)
     text= forms.CharField(max_length=3000, label='Text', required=True, widget=widgets.Textarea)
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields=['article', 'text', 'author']
+        labels={'article': 'Статья', 'text': 'Комментарий', 'author': 'Автор'}
 
